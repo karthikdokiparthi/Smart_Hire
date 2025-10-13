04a5da5e0509cac7d0cf45cebf37573e29a72b40
karthikdokiparthik <dokiparthikarthik@gmail.com>
2025-10-13 15:47:59 +0530

    chore commit

---
+++ src/main/java/com/job_portal/SmartHire/SmartHireApplication.java
@@ -0,0 +1,13 @@
+package com.job_portal.SmartHire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHireApplication.class, args);
+	}
+
+}
