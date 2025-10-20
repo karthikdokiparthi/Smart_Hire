d6caf3b137e0cfd302939f9f6f29b9ae17349afd
karthikdokiparthik <dokiparthikarthik@gmail.com>
2025-10-20 13:15:39 +0530

    feat: extend UserService interface with OTP and password reset methods
    - Added methods for sending and verifying OTPs.
    - Added methods for sending reset OTP and resetting user password.
    - Enhanced user management functionality for authentication workflows.

--- src/main/java/com/job_portal/SmartHire/service/UserService.java
+++ src/main/java/com/job_portal/SmartHire/service/UserService.java
@@ -5,5 +5,14 @@ import com.job_portal.SmartHire.dto.UserResponse;
 
 public interface UserService {
     UserResponse createUser(UserRequest userRequest);
+
     UserResponse getUser(String email);
+
+    void sendResetOtp(String email);
+
+    void resetPassword(String email,String otp,String newPassword);
+
+    void sendOtp(String email);
+
+    void verifyOtp(String email,String otp);
 }
